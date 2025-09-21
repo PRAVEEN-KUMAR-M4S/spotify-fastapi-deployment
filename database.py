f16585315186f3fae7acc022760778206dffdc7a
PraveenKumar S <praveenkumar@katenterprise.com>
2025-09-21 16:50:26 +0530

    url updates

--- database.py
+++ database.py
@@ -1,9 +1,13 @@
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import os   
 
 
-DATA_BASE_URL="postgresql://postgres:praveen@localhost:5432/music_app"
+DATA_BASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://neondb_owner:npg_ELjDoX5qF3Yx@ep-calm-leaf-ad9rqheu-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require"
+)
 engine=create_engine(DATA_BASE_URL)
 
 SessionLocal=sessionmaker(autoflush=False,bind=engine,autocommit=False)
