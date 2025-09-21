0e5d3ba3560d6e0ae9ff0beb21fedca2ac739e8d
PraveenKumar S <praveenkumar@katenterprise.com>
2025-09-21 16:22:48 +0530

    updates on deployment

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from models.base import Base
 from routes import auth, song
 from database import engine
+from mangum import Mangum
 
 app = FastAPI()
 
@@ -18,3 +19,5 @@ app.include_router(auth.router, prefix='/auth')
 app.include_router(song.router, prefix='/song')
 
 Base.metadata.create_all(engine)
+
+handler = Mangum(app)
